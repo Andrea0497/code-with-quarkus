73dfa923d115793e0e64c35fa380e061821605f1
Andrea0497 <andrea.capriello497gmail.com>
2025-04-01 21:14:30 +0200

    first commit

---
+++ src/main/java/org/acme/model/Person.java
@@ -0,0 +1,19 @@
+package org.acme.model;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Person extends PanacheEntityBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    public String name;
+    public int age;
+
+}
